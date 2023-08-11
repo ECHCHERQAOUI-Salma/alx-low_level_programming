95083db99df619a7ea1eb1f6009acd5b73bea3c3
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-11 10:27:46 -0700

    Print alphabets in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,20 +2,18 @@
 /**
  * main - Entry point
  *
- * putchar - print numbers
+ * putchar - print alphabets in reverse
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int c;
-	int n;
+	char c;
+	char n;
 
 	n = '\n';
-	for (c = 48; c <= 57; c++)
-	{
+	for (c = 'z'; c >= 'a'; c--)
 		putchar(c);
-	}
 	putchar(n);
 	return (0);
 }
