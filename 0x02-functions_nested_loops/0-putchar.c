97ec298cea176972875aceb4710b7910d88602c5
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-15 06:34:14 -0700

    Write _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * write - writes '_putchar 'to stdout
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+	return (0);
+}
