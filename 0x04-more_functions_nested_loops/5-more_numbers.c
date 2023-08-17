7b19a51a63fc4c6f512da394f34e100c71ab83b5
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-17 09:39:58 -0700

    Print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 /**
  * more_numbers - print more numbers
  *
@@ -9,21 +8,19 @@ void more_numbers(void)
 	int i;
 	int j;
 	int k;
+	int l;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			k = 0;
+			k = j / 10;
+			l = j % 10;
 			if (j >= 10)
 			{
-				k = j / 10;
-				j = j % 10;
 				_putchar(k + '0');
 			}
-			_putchar(j + '0');
-			if (k == 1)
-				j += 10;
+			_putchar(l + '0');
 		}
 		_putchar('\n');
 	}
