110ca241c5cfecd215a99e6010b0058741636844
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-17 03:44:46 -0700

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul - multiply two numbers
+ *
+ * @a: first argument
+ *
+ * @b: second argument
+ *
+ * Return: retun result
+ */
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
