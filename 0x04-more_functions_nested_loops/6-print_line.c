f8346031ce797c4cffd589a021b07fb3c76d9913
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-17 07:38:12 -0700

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - _
+ *
+ * @n: how many _
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
