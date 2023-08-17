5f503b6212fab12ae5b553525191abae5e973bef
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-17 06:47:10 -0700

    Print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,22 +9,22 @@ void more_numbers(void)
 	int i;
 	int j;
 	int k;
-	int l;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			k = 0;
 			if (j >= 10)
 			{
 				k = j / 10;
-				l = j % 10;
+				j = j % 10;
 				_putchar(k + '0');
-				_putchar(l + '0');
 			}
-			else
-				_putchar(j + '0');
+			_putchar(j + '0');
+			if (k == 1)
+				j += 10;
 		}
-		write(1, "\n", 1);
+		_putchar('\n');
 	}
 }
