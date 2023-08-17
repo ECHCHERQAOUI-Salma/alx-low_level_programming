f6a8b3967a7b063b66218267e6b357a580745c7a
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-17 08:48:40 -0700

    Print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * fizz_buzz - test
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+}
