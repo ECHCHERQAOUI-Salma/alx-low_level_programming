03ddf3a8947dcc4abf266debda36c8c62f3b5c99
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-17 08:18:48 -0700

    Print # square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print # square
+ *
+ * @size: argument
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			if (i != size)
+				_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
