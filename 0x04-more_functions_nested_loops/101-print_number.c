58da77eea193469ba25ce84b7b5bd51c3264f0e3
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-18 05:15:59 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,12 @@
  */
 void print_number(int n)
 {
-	signed int m;
-	m = n;
-	if (m < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		m *= -1;
+		n *= -1;
 	}
-	if (m / 10 != 0)
-		print_number(m / 10);
-	_putchar((m % 10) + '0');
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
