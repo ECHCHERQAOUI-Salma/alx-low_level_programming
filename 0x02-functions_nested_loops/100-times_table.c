8ce6f49f67f2a930a1352caaab1e65195ca9187a
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-19 08:05:22 -0700

    Times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_times_table - n times table
+ *
+ * @n: argument
+ */
+void print_times_table(int n)
+{
+	int i, r, j;
+	int k;
+	int l;
+	int m;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				r = i * j;
+				k = r / 10;
+				l = r % 10;
+				m = r / 100;
+				m = m + '0';
+				k = k + '0';
+				if (r >= 100)
+					k = (r / 10) % 10 + '0';
+				if (m == '0' && j != 0)
+					m = s;
+				if (m == ' ' && k == '0' && j != 0)
+					k = s;
+				if (j != 0)
+				{
+					_putchar(m);
+					_putchar(k);
+				}
+				_putchar(l + '0');
+				if (j != n)
+				{
+					_putchar(co);
+					_putchar(s);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
