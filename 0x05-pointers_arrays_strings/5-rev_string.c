4e828fa62f9437c961376d40089ceadcea3904db
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-23 07:13:14 -0700

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - print reverse string
+ *
+ * @s: argument
+ */
+void rev_string(char *s)
+{
+	int i, c = 0, j = 0;
+	char rev[10] = "         ";
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		c++;
+	for (i = c - 1; i >= 0; i--)
+	{
+		*(rev + j) = *(s + i);
+		j++;
+	}
+	for (i = 0; i < c; i++)
+		*(s + i) = *(rev + i);
+}
