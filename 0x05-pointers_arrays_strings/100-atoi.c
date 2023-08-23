2e4862fae919ae9fdff934d88f98e1d3af790bf8
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-23 09:58:01 -0700

    From string to number

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _atoi - from string to int
+ *
+ * @s: arg
+ *
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i, c = 0;
+	int j = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == '-')
+			c++;
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+			j = j * 10 + (*(s + i) - 48);
+		if (j != 0 && *(s + i + 1) == ' ')
+			break;
+	}
+	if (j == 0)
+		return (0);
+	if (c % 2 == 0)
+		return (j);
+	else
+		return (-j);
+}
