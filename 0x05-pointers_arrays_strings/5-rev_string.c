9a4b21030ed4fe11440b1a248b03e7f818f7f760
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-23 07:27:25 -0700

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,11 @@
 void rev_string(char *s)
 {
 	int i, c = 0, j = 0;
-	char rev[10] = "         ";
+        char rev[450];
 
 	for (i = 0; *(s + i) != '\0'; i++)
 		c++;
-	for (i = c - 1; i >= 0; i--)
+       	for (i = c - 1; i >= 0; i--)
 	{
 		*(rev + j) = *(s + i);
 		j++;
