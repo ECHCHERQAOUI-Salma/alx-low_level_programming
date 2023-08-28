7fde970745261e06806aab274c2cc8ae4ad96eb6
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-28 06:12:43 -0700

    Turn string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,26 @@
  */
 int _atoi(char *s)
 {
-	int i, c = 0;
+	int i, c = 0, k;
 	unsigned int j = 0;
-	int k;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == '-')
 			c++;
 		if (*(s + i) >= '0' && *(s + i) <= '9')
-			j = j * 10 + (*(s + i) - 48);
+				j = j * 10 + (*(s + i) - 48);
 		if (j != 0 && *(s + i + 1) == ' ')
 			break;
 	}
-	if (j == 0)
-		return (0);
 	if (c % 2 == 0)
-		return (j);
-	if (c % 2 != 0)
 	{
-		k = -j;
+		k = j;
+		return (k);
+	}
+	else
+	{
+		k = j * -1;
 		return (k);
 	}
 }
