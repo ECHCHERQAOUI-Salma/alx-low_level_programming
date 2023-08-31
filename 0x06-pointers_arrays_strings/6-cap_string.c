0fa06f12a19362aa03e675a03ed90406af720c3e
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-08-31 09:24:02 -0700

    Capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+11;rgb:0000/0000/0000* cap_string - capitalize words
+ *
+ * @str: string
+ *
+ * Return: pointer
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	if (str[i] <= 122 && str[i] >= 97)
+		str[i] = str[i] - 32;
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' || str[i - 1] == ',' ||
+		    str[i - 1] == ';' || str[i - 1] == '.' ||
+		    str[i - 1] == '?' || str[i - 1] == '!' ||
+		    str[i - 1] == '"' || str[i - 1] == '(' ||
+		    str[i - 1] == ')' || str[i - 1] == '{' ||
+		    str[i - 1] == '}')
+		{
+			if (str[i] <= 122 && str[i] >= 97)
+				str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
