e2f87d8c5eccd23d1cbf4d5e1e4ee8747a0363f2
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-09-01 16:42:01 -0700

    Print sums of digonals of matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,15 +8,23 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int j, sum1 = 0, sum2 = 0;
+	int i, j, sum1 = 0, sum2 = 0;
 
-	for (j = 0; j < size * size; j += size + 1)
+	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + j);
+		for (j = 0; j < size; j++)
+		{
+			sum1 =+ a[i][j];
+			break;
+		}
 	}
-	for (j = size * size - size; j >= 0; j -= size - 1)
-	{
-		sum2 += *(a + j);
-	}
+	for (i = 0; i < size; i++)
+        {
+                for (j = size - 1; j >= 0; j--)
+                {
+                        sum1 =+ a[i][j];
+                        break;
+                }
+        }
 	printf("%i, %i\n", sum1, sum2);
 }
