78d664c9e9be210b26b247640c64bc9281a5d43c
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-09-04 02:36:18 -0700

    Add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -12,15 +14,28 @@
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
+	unsigned int j;
 
 	for (i = 1; i < argc; i++)
 	{
+		if (strlen(argv[i]) > 1)
+		{
+			for (j = 0; j < strlen(argv[i]); j++)
+			{
+				if (isdigit(*(argv[i] + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
 		if (atoi(argv[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+		else
+			sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
