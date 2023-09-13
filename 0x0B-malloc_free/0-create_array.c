2372871a24ad9cf945c8423db55643bdc7fc3838
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-09-13 03:26:08 -0700

    Create array of characters

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (0);
-	a = malloc(size + 1);
+	a = malloc(size);
 	if (a == NULL)
 		return (0);
 	for (i = 0; i < size ; i++)
