c846f407097a5f3619a60a4549f72b56b7ce9fcd
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-09-16 15:30:31 -0700

    Concat strings on n characters

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s3;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 	while (*(s2 + i) != '\0')
 	{
 		l2++;
