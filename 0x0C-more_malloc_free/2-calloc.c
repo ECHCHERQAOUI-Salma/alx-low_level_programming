610f6b66cffd7b3f5039d15da345679c2ad8c582
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-09-17 06:03:04 -0700

    Allocate memory with calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+char* _memset(char*p, char c, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		p[i] = c;
+	return (p);
+}
 /**
  * _calloc - calloc
  * @nmemb: nember
@@ -8,21 +16,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int j;
 	void *p;
-	char* cp;
 
 	if (nmemb == 0 || size == 0)
 		return (0);
 	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (0);
-	j = 0;
-	cp = (char*)p;
-	for (; j < nmemb;)
-	{
-		cp[j] = 0;
-		j++;
-	}
+	_memset(p, 0, nmemb * size);
 	return (p);
 }
