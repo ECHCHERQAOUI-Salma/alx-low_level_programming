08bc75f4afafb3402a48d8669362c4a351478e12
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-09-18 06:34:39 -0700

    Create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,25 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strcpy - copy string
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * Return: *char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, c = 0;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+		c++;
+	for (i = 0; i <= c; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
+/**
  * new_dog - new struct
  * @name: name
  * @age: age
@@ -14,20 +33,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (0);
-	dog->name = malloc(sizeof(char*));
+	dog->name = malloc(sizeof(char *));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (0);
 	}
-	dog->name = name;
-	dog->owner = malloc(sizeof(char*));
-        if (dog->owner == NULL)
-        {
-                free(dog);
-                return (0);
-        }
-        dog->owner = owner;
+	_strcpy(dog->name, name);
+	dog->owner = malloc(sizeof(char *));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		return (0);
+	}
+	_strcpy(dog->owner, owner);
 	dog->age = age;
 	return (dog);
 }
