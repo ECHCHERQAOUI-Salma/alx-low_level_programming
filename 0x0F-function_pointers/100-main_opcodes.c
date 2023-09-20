efce701b1f8db9e9f4da6dae2f0e0ec6682a3dec
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-09-20 04:44:44 -0700

    Print operation code

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry
+ * @argc: c
+ * @argv: v
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int b, i;
+	char *arr;
+
+	b = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (b <= 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+	for (i = 0; i < b; i++)
+	{
+		if (i == b - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+			printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
