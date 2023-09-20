ad81207992ac4b78d64b0d7e0f1f55b8ff0895fd
ECHCHERQAOUI <echcherqaouisalma@gmail.com>
2023-09-20 10:55:43 -0700

    Print variable arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,10 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	if (separator != NULL)
+	if (separator != NULL && n != 0)
 	{
 		for (i = 0; i < n - 1; i++)
 			printf("%d%c ", va_arg(ap, int), *separator);
-		printf("%d\n", va_arg(ap, int));
+		printf("%d", va_arg(ap, int));
 	}
+	printf("\n");
 }
