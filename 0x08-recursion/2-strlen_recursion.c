24e1f1fb56c044b2d9f4fde1adbe3d7c58fc4916
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-10-26 04:59:41 -0700

    Count string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - string length
+ * @s: string
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s != '\0')
+		i = _strlen_recursion(s + 1);
+	else
+		i = -1;
+	return (i + 1);
+}
