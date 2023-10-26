9a763c20b0e17606ca2c4255bf751b1ca348a07e
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-10-26 03:58:03 -0700

    Print Reversed string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Print string in recursion
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*(s + 1) != '\0')
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
