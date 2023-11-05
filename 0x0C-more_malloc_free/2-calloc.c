aaa57fe70e873da72dc29550a450b115f5db69fd
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-05 04:57:01 -0800

    Calloc a memory

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - calloc
+ * @nmemb: elts
+ * @size: size
+ * Return: void*
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *c;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	c = malloc(size * nmemb);
+	if (c == NULL)
+		return (0);
+	for (i = 0; i < size; i++)
+		((char *)c)[i] = '0';
+	return (c);
+}
