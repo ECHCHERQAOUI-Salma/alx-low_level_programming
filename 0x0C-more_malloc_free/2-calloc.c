8672c912ae9772f7781650ed915d1069b9f2ddd0
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-05 12:34:18 -0800

    Calloc memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (c == NULL)
 		return (0);
 	c1 = (char *)c;
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		c1[i] = '\0';
 	return (c1);
 }
