6c3fff8a6b2b65f1a071a52c07c8d800db2fc002
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-21 02:18:47 -0800

    Print list elts

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,23 @@
 size_t print_list(const list_t *h)
 {
 	int c = 1;
+	list_t *np;
 
+	if (h == NULL)
+		return (0);
 	if (h->str == NULL)
 		printf("[0] nil\n");
 	else
 		printf("[%d] %s\n", h->len, h->str);
-	if (h->next == NULL)
-		return(c);
-	c++;
-	if (h->next->str == NULL)
-		printf("[0] nil\n");
-	else
-		printf("[%d] %s\n", h->next->len, h->next->str);
+	np = h->next;
+	while (np != NULL)
+	{
+		c++;
+		if (np->str == NULL)
+			printf("[0] nil\n");
+		else
+			printf("[%d] %s\n", np->len, np->str);
+		np = np->next;
+	}
 	return (c);
 }
