272e0959b727261c23d32089e078e86457a39d6d
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-22 11:46:21 -0800

    Free a list with null head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - freeeee
+ * @head: head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	listint_t *next;
+
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
