6f0fb6b58d1d459be0a6e18977b931d0c901e753
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-22 03:16:50 -0800

    Free a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - freeeee
+ * @head: head of list
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
