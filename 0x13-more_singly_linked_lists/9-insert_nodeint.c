029eb00bf4b99363d37a446018582500ceb1e51a
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-23 08:25:29 -0800

    Insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,33 +12,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current, *next, *replace;
 	unsigned int i;
 
-	if (*head == NULL || n < 0)
+	if (head == NULL || n < 0)
 		return (0);
 	current = *head;
-	for (i = 0; i <= idx; i++)
+	for (i = 0; i <= idx + 1; i++)
 	{
-		while (current != NULL)
+		next = malloc(sizeof(listint_t));
+		if (next == NULL)
+			return (0);
+		next->n = n;
+		if (idx == 0)
 		{
-			next = malloc(sizeof(listint_t));
-			if (next == NULL)
-				return (0);
-			next->n = n;
-			if (idx == 0)
-			{
-				next->next = *head;
-				*head = next;
-				return (*head);
-			}
-			else if (i == idx - i)
-			{
-				replace = current->next;
-				next->next = replace;
-				current->next = next;
-				return (next);
-			}
-			current = current->next;
+			next->next = *head;
+			*head = next;
+			return (*head);
 		}
-		return (0);
+		else if (i == idx - i)
+		{
+			replace = current->next;
+			next->next = replace;
+			current->next = next;
+			return (next);
+		}
+		current = current->next;
 	}
 	return (0);
 }
