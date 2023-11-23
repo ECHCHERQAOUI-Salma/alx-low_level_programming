ba4f1fdf2bf64a237eb585a674e6b66b4202cfd0
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-23 07:42:58 -0800

    Inset node at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_nodeint_at_index - insert at index
+ * @head: h
+ * @idx: index
+ * @n: int
+ * Return: pointer
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current, *next, *replace;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (0);
+	current = *head;
+	for (i = 0; i <= idx; i++)
+	{
+		while (current != NULL)
+		{
+			if (i == idx - i)
+			{
+				next = malloc(sizeof(listint_t));
+				replace = current->next;
+				next->next = replace;
+				next->n = n;
+				current->next = next;
+				return (next);
+			}
+			current = current->next;
+		}
+		return (0);
+	}
+	return (0);
+}
