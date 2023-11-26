b43af9a2c003a9dee551a0d3236b52050ea75aaf
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-26 00:03:16 -0800

    Turn binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,16 +7,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int l = 0, s = 0, i, p = 1;
-	int n;
+	unsigned int l = 0, s = 0,  p = 1;
+	int n, i;
 
 	if (b == NULL)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 		l++;
-	for (i = l; i > 0; i--)
+	for (i = l - 1; i >= 0; i--)
 	{
-		n = *(b + i);
+		n = *(b + i) - 48;
 		if (n != 0 && n != 1)
 			return (0);
 		s = s + (n * p);
