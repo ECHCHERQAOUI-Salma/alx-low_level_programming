25e986d703ce9219e7ceef2c060cebbdc064cae1
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-28 00:37:12 -0800

    write file content to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+ * read_textfile - print file content
+ * @filename: f
+ * @letters: l
+ * Return: int
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char *b;
+	ssize_t c, e;
+
+	b = malloc(sizeof(char) * letters + 1);
+	if (filename == NULL)
+	{
+		free(b);
+		return (0);
+	}
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		free(b);
+		return (0);
+	}
+	c = fread(b, 1, letters, fp);
+	e = write(1, b, c);
+	free(b);
+	fclose(fp);
+	if (e != -1)
+		return (e);
+	else
+		return (0);
+}
