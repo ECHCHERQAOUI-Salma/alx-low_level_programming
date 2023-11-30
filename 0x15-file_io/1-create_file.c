42e3e9026adac5272212f2bd25244b5be03bb013
ECHCHERQAOUI SALMA <echcherqaouisalma@gmail.com>
2023-11-30 07:15:46 -0800

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,20 +18,28 @@ int create_file(const char *filename, char *text_content)
 	if (access(filename, F_OK) == 0)
 	{
 		fp = fopen(filename, "w");
+		if (fp == NULL)
+			return (-1);
 	}
 	else
 	{
-		fd = open(filename, O_RDONLY | O_CREAT, 0600);
+		fd = open(filename, O_RDWR | O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
-		fp = fdopen(fd, "r");
+		fp = fdopen(fd, "w");
 		if (fp == NULL)
+		{
+			close(fd);
 			return (-1);
+		}
 		if (text_content != NULL)
 		{
 			n = fprintf(fp, "%s", text_content);
 			if (n < 0)
+			{
+				fclose(fp);
 				return (-1);
+			}
 		}
 	}
 	fclose(fp);
